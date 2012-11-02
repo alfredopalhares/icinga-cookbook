602d51e5f9307b4b03c241b4bf90700755f93216
Alfredo Palhares <masterkorp@masterkorp.net>
2012-11-02 13:35:49 +0000

    Icinga service resource

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,3 +2,13 @@ package "icinga" do
   action :install
 end
 
+service "icinga" do
+  supports(
+    :start => true,
+    :stop => true,
+    :restart => true,
+    :status => true,
+    :reload => true
+  )
+  action [:enable, :start]
+end
