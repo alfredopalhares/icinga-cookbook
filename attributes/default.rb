44f0ec909a5396c2a8697795d78719d6a0dc621b
Alfredo Palhares <masterkorp@masterkorp.net>
2012-11-03 21:36:16 +0000

    Main Icinga configuration file template. Set all the relevant settings as attributes. Icinga service reload notification.

---
+++ attributes/default.rb
@@ -0,0 +1,54 @@
+default["icinga"]["conf_dir"] = "/etc/icinga"
+default["icinga"]["nagios_plugins"] = "/etc/nagios-plugins/config"
+default["icinga"]["object_dir"] = "#{node["icinga"]["conf_dir"]}/objects"
+default["icinga"]["module_dir"] = "#{node["icinga"]["conf_dir"]}/modules"
+
+default["icinga"]["user"] = "nagios"
+default["icinga"]["group"] = "nagios"
+
+default["icinga"]["check_external_commands"] = 1
+default["icinga"]["command_check_interval"] = -1
+
+default["icinga"]["enable_notifications"] = 1
+default["icinga"]["accept_passive_host_checks"] = 1
+
+default["icinga"]["process_performance_data"] = 0
+default["icinga"]["perfdata_file_mode"] = "a"
+default["icinga"]["perfdata_file_processing_interval"] = 0
+
+default["icinga"]["obsess_over_services"] = 0
+default["icinga"]["ocsp_command"] = "somecommand"
+default["icinga"]["obsess_over_hosts"] = 0
+default["icinga"]["ochp_command"] = "somecommand"
+
+default["icinga"]["translate_passive_host_checks"] = 0
+default["icinga"]["passive_host_checks_are_soft"] = 0
+
+default["icinga"]["check_for_orphaned_services"] = 1
+default["icinga"]["check_for_orphaned_hosts"] = 1
+
+default["icinga"]["service_check_timeout_state"] = "u"
+default["icinga"]["check_service_freshness"] = 1
+
+default["icinga"]["service_freshness_check_interval"] = 60
+default["icinga"]["check_host_freshness"] = 0
+default["icinga"]["host_freshness_check_interval"] = 60
+default["icinga"]["additional_freshness_latency"] = 15
+
+default["icinga"]["enable_flap_detection"] = 1
+default["icinga"]["low_service_flap_threshold"] = 5.0
+default["icinga"]["high_service_flap_threshold"] = 20.0
+default["icinga"]["low_host_flap_threshold"] = 5.0
+default["icinga"]["high_host_flap_threshold"] = 20.0
+
+default["icinga"]["date_format"] = "iso8601"
+
+default["icinga"]["stalking_event_handlers_for_hosts"] = 0
+default["icinga"]["stalking_event_handlers_for_services"] = 0
+default["icinga"]["stalking_notifications_for_hosts"] = 0
+default["icinga"]["stalking_notifications_for_services"] = 0
+
+default["icinga"]["admin_email"] = "root@localhost"
+default["icinga"]["admin_pager"] = "pageroot@localhost"
+
+default["icinga"]["enable_environment_macros"] = 1
--- recipes/server.rb
+++ recipes/server.rb
@@ -12,3 +12,57 @@ service "icinga" do
   )
   action [:enable, :start]
 end
+
+template "#{node["icinga"]["conf_dir"]}/icinga.cfg" do
+  source "icinga.cfg.erb"
+  owner "root"
+  group "root"
+  mode 00644
+  variables(
+    :conf_dir => node["icinga"]["conf_dir"],
+    :nagios_plugins => node["icinga"]["nagios_plugins"],
+    :object_dir => node["icinga"]["object_dir"],
+    :module_dir => node["icinga"]["module_dir"],
+    :user => node["icinga"]["user"],
+    :group => node["icinga"]["group"] || node["icinga"]["user"],
+    :check_external_commands => node["icinga"]["check_external_commands"],
+    :command_check_interval => node["icinga"]["command_check_interval"],
+    :enable_notifications => node["icinga"]["enable_notifications"],
+    :accept_passive_host_checks => node["icinga"]["accept_passive_host_checks"],
+    :process_performance_data => node["icinga"]["process_performance_data"],
+    :perfdata_file_mode => node["icinga"]["perfdata_file_mode"],
+    :perfdata_file_processing_interval => node["icinga"]["perfdata_file_processing_interval"],
+    :obsess_over_services => node["icinga"]["obsess_over_services"],
+    :ocsp_command => node["icinga"]["ocsp_command"],
+    :obsess_over_hosts => node["icinga"]["obsess_over_hosts"],
+    :ochp_command => node["icinga"]["ochp_command"],
+    :translate_passive_host_checks => node["icinga"]["translate_passive_host_checks"],
+    :passive_host_checks_are_soft => node["icinga"]["passive_host_checks_are_soft"],
+    :check_for_orphaned_services => node["icinga"]["check_for_orphaned_services"],
+    :check_for_orphaned_hosts => node["icinga"]["check_for_orphaned_hosts"],
+    :service_check_timeout_state => node["icinga"]["service_check_timeout_state"],
+    :check_service_freshness => node["icinga"]["check_service_freshness"],
+    :service_freshness_check_interval => node["icinga"]["service_freshness_check_interval"],
+    :check_host_freshness => node["icinga"]["check_host_freshness"],
+    :host_freshness_check_interval => node["icinga"]["host_freshness_check_interval"],
+    :additional_freshness_latency => node["icinga"]["additional_freshness_latency"],
+    :enable_flap_detection => node["icinga"]["enable_flap_detection"],
+    :low_service_flap_threshold => node["icinga"]["low_service_flap_threshold"],
+    :high_service_flap_threshold => node["icinga"]["high_service_flap_threshold"],
+    :low_host_flap_threshold => node["icinga"]["low_host_flap_threshold"],
+    :high_host_flap_threshold => node["icinga"]["high_host_flap_threshold"],
+    :date_format => node["icinga"]["date_format"],
+    :stalking_event_handlers_for_hosts => node["icinga"]["stalking_event_handlers_for_hosts"],
+    :stalking_event_handlers_for_services => node["icinga"]["stalking_event_handlers_for_services"],
+    :stalking_notifications_for_hosts => node["icinga"]["stalking_notifications_for_hosts"],
+    :stalking_notifications_for_services => node["icinga"]["stalking_notifications_for_services"],
+    :admin_email => node["icinga"]["admin_email"],
+    :admin_pager => node["icinga"]["admin_pager"],
+    :enable_environment_macros => node["icinga"]["enable_environment_macros"]
+  )
+  notifies(
+    :reload,
+    resources(:service => "icinga"),
+    :immediately
+  )
+end
