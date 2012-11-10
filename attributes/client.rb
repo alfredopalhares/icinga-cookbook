98aa1490639cc3ca13679dafe8031d5fa4e859c5
Alfredo Palhares <masterkorp@masterkorp.net>
2012-11-10 20:53:00 +0000

    Automatic search for Icinga servers. Adds the found servers to the allowed hosts array on nrpe so those servers can talk to it

--- attributes/client.rb
+++ attributes/client.rb
@@ -2,7 +2,6 @@ default["icinga"]["nrpe"]["conf_dir"] = "/etc/nagios"
 
 default["icinga"]["nrpe"]["pidfile"] = "/var/run/nagios/nrpe.pid"
 default["icinga"]["nrpe"]["port"] = 5666
-default["icinga"]["nrpe"]["allowed_hosts"] = ["127.0.0.1"]
 default["icinga"]["nrpe"]["dont_blame_nrpe"] = 0
 default["icinga"]["nrpe"]["command_timeout"] = 60
 default["icinga"]["nrpe"]["connection_timeout"] = 360
--- recipes/client.rb
+++ recipes/client.rb
@@ -12,6 +12,20 @@ service "nagios-nrpe-server" do
   action [:enable, :start]
 end
 
+allowed_hosts = ["127.0.0.1"]
+if Chef::Config[:solo]
+  Chef::Log.warn("You need to be able to search to find the icinga servers!")
+else
+  servers = search(:node, "recipe*:icinga\\:\\:server")
+  servers.each do |server|
+  allowed_hosts.push(server["ipaddress"])
+    if node["ipaddress"] != server["ipaddress"]
+      allowed_hosts.push(server["ipaddress"])
+    end
+  end
+end
+  Chef::Log.warn(allowed_hosts.to_s)
+
 template "#{node["icinga"]["nrpe"]["conf_dir"]}/nrpe.cfg" do
   source "nrpe.cfg.erb"
   owner "root"
@@ -22,7 +36,7 @@ template "#{node["icinga"]["nrpe"]["conf_dir"]}/nrpe.cfg" do
     :port => node["icinga"]["nrpe"]["port"],
     :user => node["icinga"]["user"],
     :group => node["icinga"]["group"] || node["icinga"]["group"],
-    :allowed_hosts => node["icinga"]["nrpe"]["allowed_hosts"].join(","),
+    :allowed_hosts => allowed_hosts,
     :dont_blame_nrpe => node["icinga"]["nrpe"]["dont_blame_nrpe"],
     :command_timeout => node["icinga"]["nrpe"]["command_timeout"],
     :connection_timeout => node["icinga"]["nrpe"]["connection_timeout"],
