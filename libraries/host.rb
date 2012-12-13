27e5512550523d120005bc882718b3eb591469fb
Alfredo Palhares <masterkorp@masterkorp.net>
2012-12-13 16:43:03 +0000

    Replace Host attributes in a global Hash. Attribute validation stays the same with the new set_in_hash method. This makes it easy for future methods that want to cycle trough all the attributes.

--- libraries/host.rb
+++ libraries/host.rb
@@ -6,53 +6,69 @@ class Chef
       def initialize(name, collection=nil, node=nil)
         super(name, collection, node)
         @resource_name = :icinga_host
-        @host_name = name
-        @alias = nil
-        @display_name = nil
-        @address = nil
-        @address6 = nil
-        @display_name = nil
-        @parents = nil
-        @hostgroups = nil
-        @check_command = nil
-        @initial_state = nil
-        @max_check_attempts = nil
-        @check_interval = nil
-        @retry_interval = nil
-        @active_checks_enabled = nil
-        @passive_checks_enabled = nil
-        @check_period = nil
-        @obsess_over_host = nil
-        @check_freshness = nil
-        @freshness_threshold = nil
-        @event_handler = nil
-        @low_flap_threshould = nil
-        @high_flap_threshould = nil
-        @flap_detetction_enabled = nil
-        @failure_prediction_enabled = nil
-        @process_perf_data = nil
-        @retain_status_information = nil
-        @retain_nonstatus_information = nil
-        @contacts = nil
-        @contact_groups = nil
-        @notification_interval = nil
-        @first_notification_delay = nil
-        @notification_period = nil
-        @notification_options = nil
-        @notifications_enabled = nil
-        @stalking_options = nil
-        @notes = nil
-        @notes_url = nil
-        @action_url = nil
-        @icon_image = nil
-        @icon_image_alt = nil
-        @statusmap_image = nil
-        # Vars can't start with a number
-        @_2d_coords = nil
+        @attributes = {
+          "host_name" => name,
+          "display_name" => nil,
+          "address" => nil,
+          "address6" => nil,
+          "display_name" => nil,
+          "parents" => nil,
+          "hostgroups" => nil,
+          "check_command" => nil,
+          "initial_state" => nil,
+          "max_check_attempts" => nil,
+          "check_interval" => nil,
+          "retry_interval" => nil,
+          "active_checks_enabled" => nil,
+          "passive_checks_enabled" => nil,
+          "check_period" => nil,
+          "obsess_over_host" => nil,
+          "check_freshness" => nil,
+          "freshness_threshold" => nil,
+          "event_handler" => nil,
+          "low_flap_threshould" => nil,
+          "high_flap_threshould" => nil,
+          "flap_detetction_enabled" => nil,
+          "failure_prediction_enabled" => nil,
+          "process_perf_data" => nil,
+          "retain_status_information" => nil,
+          "retain_nonstatus_information" => nil,
+          "contacts" => nil,
+          "contact_groups" => nil,
+          "notification_interval" => nil,
+          "first_notification_delay" => nil,
+          "notification_period" => nil,
+          "notification_options" => nil,
+          "notifications_enabled" => nil,
+          "stalking_options" => nil,
+          "notes" => nil,
+          "notes_url" => nil,
+          "action_url" => nil,
+          "icon_image" => nil,
+          "icon_image_alt" => nil,
+          "statusmap_image" => nil,
+          # Vars can't start with a number
+          "_2d_coords" => nil
+        }
       end
 
+      private
+
+      # Sets the arg in the attributes hash if the validations pass
+      # Uses the validate from Mixin::ParamsValidate
+      def set_in_hash(symbol, arg, validation)
+        if arg != nil
+           opts = validate({ symbol => arg }, { symbol => validation })
+          if @attributes.has_key?(symbol.to_s())
+            @attributtes[symbol.to_s()] = opts[symbol]
+          end
+        end
+      end
+
+      public
+
       def alias(arg=nil)
-        set_or_return(
+        set_in_hash(
           :alias,
           arg,
           :kind_of => String
@@ -60,7 +76,7 @@ class Chef
       end
 
       def display_name(arg=nil)
-        set_or_return(
+        set_in_hash(
           :display_name,
           arg,
           :kind_of => String
@@ -68,7 +84,7 @@ class Chef
       end
 
       def address(arg=nil)
-        set_or_return(
+        set_in_hash(
           :address,
           arg,
           :kind_of => String
@@ -76,7 +92,7 @@ class Chef
       end
 
       def address6(arg=nil)
-        set_or_return(
+        set_in_hash(
           :address6,
           arg,
           :kind_of => String
@@ -84,7 +100,7 @@ class Chef
       end
 
       def parents(arg=nil)
-        set_or_return(
+        set_in_hash(
           :parents,
           arg,
           :kind_of => Array
@@ -92,7 +108,7 @@ class Chef
       end
 
       def hostgroups(arg=nil)
-        set_or_return(
+        set_in_hash(
           :hostgroups,
           arg,
           :kind_of => Array
@@ -100,7 +116,7 @@ class Chef
       end
 
       def check_command(arg=nil)
-        set_or_return(
+        set_in_hash(
           :address,
           arg,
           :kind_of => String
@@ -108,7 +124,7 @@ class Chef
       end
 
       def initial_state(arg=nil)
-        set_or_return(
+        set_in_hash(
           :initial_state,
           arg,
           :kind_of => String
@@ -116,7 +132,7 @@ class Chef
       end
 
       def max_check_attempts(arg=nil)
-        set_or_return(
+        set_in_hash(
           :max_check_attempts,
           arg,
           :kind_of => Integer
@@ -124,7 +140,7 @@ class Chef
       end
 
       def check_interval(arg=nil)
-        set_or_return(
+        set_in_hash(
           :check_interval,
           arg,
           :kind_of => Integer
@@ -132,7 +148,7 @@ class Chef
       end
 
       def retry_interval(arg=nil)
-        set_or_return(
+        set_in_hash(
           :retry_interval,
           arg,
           :kind_of => Integer
@@ -140,7 +156,7 @@ class Chef
       end
 
       def active_checks_enabled(arg=nil)
-        set_or_return(
+        set_in_hash(
           :active_checks_enabled,
           arg,
           :kind_of => [TrueClass, FalseClass]
@@ -148,7 +164,7 @@ class Chef
       end
 
       def passive_checks_enabled(arg=nil)
-        set_or_return(
+        set_in_hash(
           :passive:checks_enabled,
           arg,
           :kind_of => [TrueClass, FalseClass]
@@ -156,7 +172,7 @@ class Chef
       end
 
       def check_period(arg=nil)
-        set_or_return(
+        set_in_hash(
           :check_period,
           arg,
           :kind_of => String
@@ -164,7 +180,7 @@ class Chef
       end
 
       def obsess_over_host(arg=nil)
-        set_or_return(
+        set_in_hash(
           :obsess_over_host,
           arg,
           :kind_of => [ TrueClass, FalseClass ]
@@ -172,7 +188,7 @@ class Chef
       end
 
       def check_freshness(arg=nil)
-        set_or_return(
+        set_in_hash(
           :check_freshness,
           arg,
           :kind_of => [ TrueClass, FalseClass ]
@@ -180,182 +196,182 @@ class Chef
       end
 
       def freshness_threshold(arg=nil)
-        set_or_return(
+        set_in_hash(
           :freshness_threshold,
           arg,
           :kind_of => Integer
       end
 
       def event_handler(arg=nil)
-        set_or_return(
+        set_in_hash(
           :event_handler,
           arg,
           :kind_of => String
       end
 
       def event_handler_enabled(arg=nil)
-        set_or_return(
+        set_in_hash(
           :event_handler_enabled,
           arg,
           :kind_of => [ TrueClass, FalseClass ]
       end
 
       def low_flap_threshold(arg=nil)
-        set_or_return(
+        set_in_hash(
           :low_flap_threshold,
           arg,
           :kind_of => Integer
       end
 
       def high_flap_threshold(arg=nil)
-        set_or_return(
+        set_in_hash(
           :high_flap_threshold,
           arg,
           :kind_of => Integer
       end
 
       def flap_detection_enabled(arg=nil)
-        set_or_return(
+        set_in_hash(
           :flap_detection_enabled,
           arg,
           :kind_of => [ TrueClass, FalseClass ]
       end
 
       def flap_detection_options(arg=nil)
-        set_or_return(
+        set_in_hash(
           :flap_detection_otions,
           arg,
           :kind_of => Array
       end
 
       def failure_prediction_enabled(arg=nil)
-        set_or_return(
+        set_in_hash(
           :failure_prediction_enabled,
           arg,
           :kind_of => [ TrueClass, FalseClass ]
       end
 
       def process_perf_data(arg=nil)
-        set_or_return(
+        set_in_hash(
           :process_perf_data,
           arg,
           :kind_of => [ TrueClass, FalseClass ]
       end
 
       def retain_status_information(arg=nil)
-        set_or_return(
+        set_in_hash(
           :retain_status_information,
           arg,
           :kind_of => [ TrueClass, FalseClass ]
       end
 
       def retain_nonstatus_information(arg=nil)
-        set_or_return(
+        set_in_hash(
           :retain_nonstatus_information,
           arg,
           :kind_of => [ TrueClass, FalseClass ]
       end
 
       def contacts(arg=nil)
-        set_or_return(
+        set_in_hash(
           :contacts,
           arg,
           :kind_of => Array
       end
 
       def contact_groups(arg=nil)
-        set_or_return(
+        set_in_hash(
           :contact_groups,
           arg,
           :kind_of => Array
       end
 
       def notification_interval(arg=nil)
-        set_or_return(
+        set_in_hash(
           :notification_interval,
           arg,
           :kind_of => Integer
       end
 
       def first_notification_delay(arg=nil)
-        set_or_return(
+        set_in_hash(
           :first_notification_delay,
           arg,
           :kind_of => Integer
       end
 
       def notification_period(arg=nil)
-        set_or_return(
+        set_in_hash(
           :notification_period,
           arg,
           :kind_of => String
       end
 
       def notification_options(arg=nil)
-        set_or_return(
+        set_in_hash(
           :notification_options,
           arg,
           :kind_of => Array
       end
 
       def notifications_enabled(arg=nil)
-        set_or_return(
+        set_in_hash(
           :notification_enabled,
           arg,
           :kind_of => [ TrueClass, FalseClass ]
       end
 
       def stalking_options(arg=nil)
-        set_or_return(
+        set_in_hash(
           :notification_interval,
           arg,
           :kind_of => Array
       end
 
       def notes(arg=nil)
-        set_or_return(
+        set_in_hash(
           :notes,
           arg,
           :kind_of => String
       end
 
       def notes_url(arg=nil)
-        set_or_return(
+        set_in_hash(
           :notes_url,
           arg,
           :kind_of => String
       end
 
       def action_url(arg=nil)
-        set_or_return(
+        set_in_hash(
           :action_url,
           arg,
           :kind_of => String
       end
 
       def icon_image(arg=nil)
-        set_or_return(
+        set_in_hash(
           :icon_image,
           arg,
           :kind_of => String
       end
 
       def icon_image_alt(arg=nil)
-        set_or_return(
+        set_in_hash(
           :icon_image_alt,
           arg,
           :kind_of => String
       end
 
       def statusmap_image(arg=nil)
-        set_or_return(
+        set_in_hash(
           :statusmap_image,
           arg,
           :kind_of => String
       end
 
       def _2d_coords(arg=nil)
-        set_or_return(
+        set_in_hash(
           :_2d_coords,
           arg,
           :kind_of => Array
